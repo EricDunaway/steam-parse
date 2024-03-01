ef0fe12285af6eef326deab0c3a4d99f0d815825
Eric Dunaway <4960630+EricDunaway@users.noreply.github.com>
2024-03-01 15:57:24 -0700

    removed depreciated use for parse error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use vdf_parse::parse_string;
 
-
 fn main() {
- println!("Most of the work is in the vdf-parse library");
+    println!("Most of the work is in the vdf-parse library");
 }
--- vdf-parse/src/lib.rs
+++ vdf-parse/src/lib.rs
@@ -1,17 +1,16 @@
-use winnow::{
-    branch::alt,
-    bytes::{tag, take, take_until1},
-    error::VerboseError,
-    number,
-    sequence::{self, preceded},
-    combinator,
-    IResult,
-    prelude::*,
-};
 use std::{
     borrow::Cow,
     collections::HashMap,
     str::{self, from_utf8},
+};
+use winnow::{
+    branch::alt,
+    bytes::{tag, take, take_until1},
+    combinator,
+    error::VerboseError,
+    number,
+    sequence::{self, preceded},
+    IResult, Parser,
 };
 
 #[derive(PartialEq, Eq, Debug)]
@@ -21,72 +20,63 @@ pub enum MapValue<'i> {
     Object(HashMap<Cow<'i, str>, MapValue<'i>>),
 }
 
-type IResutlCowStrMapValue<'a> = IResult<&'a [u8], (Cow<'a,str>, MapValue<'a>), VerboseError<&'a [u8]>>;
+type IResutlCowStrMapValue<'a> =
+    IResult<&'a [u8], (Cow<'a, str>, MapValue<'a>), VerboseError<&'a [u8]>>;
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct VdfFile<'i> {
     entries: HashMap<Cow<'i, str>, MapValue<'i>>,
 }
 
-
-pub fn parse_string(input: &[u8]) -> IResult<&[u8],Cow<str>,VerboseError<&[u8]>> {
-    let bytes = sequence::terminated(take_until1(&[0x00][..]), take(1usize)).context("parseSring").parse_next(input)?;
-    Ok((bytes.0,Cow::Borrowed(from_utf8(bytes.1).unwrap())))      
+pub fn parse_string(input: &[u8]) -> IResult<&[u8], Cow<str>, VerboseError<&[u8]>> {
+    let bytes = sequence::terminated(take_until1(&[0x00][..]), take(1usize))
+        .context("parseSring")
+        .parse_next(input)?;
+    Ok((bytes.0, Cow::Borrowed(from_utf8(bytes.1).unwrap())))
 }
 
 #[allow(clippy::needless_lifetimes)]
-pub fn parse_integer_entity<'i>(
-    input: &'i [u8],
-) -> IResutlCowStrMapValue {
-    winnow::error::context(
-        "parse_integer_entity",
-        sequence::tuple((
-            preceded(tag(b"\x02"), parse_string),
-            combinator::map(number::complete::le_u32, MapValue::Number),
-        )),
-    )(input)
+pub fn parse_integer_entity<'i>(input: &'i [u8]) -> IResutlCowStrMapValue {
+    (
+        preceded(tag(b"\x02"), parse_string),
+        combinator::map(number::complete::le_u32, MapValue::Number),
+    )
+        .context("parse_integer_entity")
+        .parse_next(input)
 }
 
 #[allow(clippy::needless_lifetimes)]
-pub fn parse_string_entity<'i>(
-    input: &'i [u8],
-) -> IResutlCowStrMapValue {
-    winnow::error::context(
-        "parse_string_entity",
-        sequence::tuple((
-            preceded(tag(b"\x01"), parse_string),
-            combinator::map(parse_string, MapValue::String),
-        )),
-    )(input)
+pub fn parse_string_entity<'i>(input: &'i [u8]) -> IResutlCowStrMapValue {
+    (
+        preceded(tag(b"\x01"), parse_string),
+        combinator::map(parse_string, MapValue::String),
+    )
+        .context("parse_string_entity")
+        .parse_next(input)
 }
 
 #[allow(clippy::needless_lifetimes)]
-pub fn parse_hash_entity<'i>(
-    input: &'i [u8],
-) -> IResutlCowStrMapValue {
-    winnow::error::context(
-        "parse_hash_entity",
-        sequence::delimited(
-            tag(b"\x00"),
-            sequence::tuple((
-                parse_string,
-                combinator::map(winnow::multi::many0(parse_map_value), |x: Vec<(Cow<'_, str>, MapValue<'_>)>| {
-                    MapValue::Object(x.into_iter().collect())
-                }),
-            )),
-            tag(b"\x08"),
+pub fn parse_hash_entity<'i>(input: &'i [u8]) -> IResutlCowStrMapValue {
+    sequence::delimited(
+        tag(b"\x00"),
+        (
+            parse_string,
+            combinator::map(
+                winnow::multi::many0(parse_map_value),
+                |x: Vec<(Cow<'_, str>, MapValue<'_>)>| MapValue::Object(x.into_iter().collect()),
+            ),
         ),
-    )(input)
+        tag(b"\x08"),
+    )
+    .context("parse_hash_entity")
+    .parse_next(input)
 }
 
 #[allow(clippy::needless_lifetimes)]
-pub fn parse_map_value<'i>(
-    input: &'i [u8],
-) -> IResutlCowStrMapValue {
-    winnow::error::context(
-        "parse_map_entity",
-        alt((parse_string_entity, parse_integer_entity, parse_hash_entity)),
-    )(input)
+pub fn parse_map_value<'i>(input: &'i [u8]) -> IResutlCowStrMapValue {
+    alt((parse_string_entity, parse_integer_entity, parse_hash_entity))
+        .context("parse_map_entity")
+        .parse_next(input)
 }
 
 #[cfg(test)]
