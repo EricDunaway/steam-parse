c2cbc49c5ac1f5ee3c04d10f87e670a60c98ab83
Eric Dunaway <4960630+EricDunaway@users.noreply.github.com>
2024-03-17 11:26:49 -0600

    Add serde and clap dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
-use vdf_parse::parse_string;
+use serde_json::to_string_pretty;
+use std::{collections::HashMap, fs::File, io::Read};
+use vdf_parse::parse_hash_entity;
+
+use clap::Parser;
+
+/// Simple program to parse a VDF file and print the result
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name and path of the file to read
+    #[arg(short, long)]
+    input: String,
+}
 
 fn main() {
     println!("Most of the work is in the vdf-parse library");
+    let args = Args::parse();
+    println!("Reading file: {}", args.input);
+
+    // Read file into &mut &[u8] and call parse_hash_entity then print the result
+    let mut vdf_hashmap = HashMap::new();
+    {
+        let mut vdf_file = File::open(args.input).unwrap();
+        let size = vdf_file.metadata().unwrap().len() as usize;
+        let mut buffer: Vec<u8> = Vec::with_capacity(size);
+        vdf_file
+            .read_to_end(&mut buffer)
+            .expect("Eror Reading file");
+        let mut buffer = buffer.as_slice();
+        let result = match parse_hash_entity(&mut buffer){
+            Ok(actual) => actual,
+            Err(e) => panic!("Error parsing VDF file: {}", e),
+        };
+        vdf_hashmap.insert(result.0, result.1);
+    }
+    let json = to_string_pretty(&vdf_hashmap).unwrap();
+    println!("{}", json);
 }
--- vdf-parse/src/lib.rs
+++ vdf-parse/src/lib.rs
@@ -1,69 +1,122 @@
 use std::{collections::HashMap, str::from_utf8};
 use winnow::{
     binary,
-    combinator::{delimited, preceded, repeat, terminated, dispatch, fail, peek},
+    combinator::{delimited, preceded, repeat, terminated, dispatch, fail, peek, trace},
     error::ContextError,
-    token::{ literal, take_until, any},
+    token::{ take_until, any},
     PResult, Parser,
 };
+use serde::{Serialize, Serializer, ser::SerializeMap, Deserialize};
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Debug, Deserialize, Clone)]
 pub enum MapValue {
-    Number(u32),
+    Number32(u32),
+    Number64(u64),
+    NumberFloat(f32),
     String(String),
     Object(HashMap<String, MapValue>),
 }
 
-#[derive(PartialEq, Eq, Debug)]
+impl Serialize for MapValue {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            MapValue::Number32(num) => serializer.serialize_u32(num),
+            MapValue::Number64(num) => serializer.serialize_u64(num),
+            MapValue::NumberFloat(num) => serializer.serialize_f32(num),
+            MapValue::String(ref s) => serializer.serialize_str(s),
+            MapValue::Object(ref obj) => {
+                // Here, use serialize_map instead of serialize_struct
+                let mut map = serializer.serialize_map(Some(obj.len()))?;
+                for (key, value) in obj {
+                    map.serialize_entry(key, value)?;
+                }
+                map.end()
+            },
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 pub struct VdfFile {
     entries: HashMap<String, MapValue>,
 }
 
 pub fn parse_string<'i>(input: &mut &'i [u8]) -> PResult<String, ContextError<&'i str>> {
+    trace("parseString",
     terminated(take_until(0.., 0x00), 0x00)
         .context("parseString Before Map")
         .map(|bytes| from_utf8(bytes).unwrap().to_string())
         .context("parseSring")
+    ).parse_next(input)
+}
+
+pub fn parse_integer32_entity<'i>(
+    input: &mut &'i [u8],
+) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parse_integer32_entity",
+    (
+        preceded(b"\x02", parse_string),
+        binary::le_u32.map(MapValue::Number32),
+    )
+        .context("parse_integer32_entity"))
         .parse_next(input)
 }
 
-pub fn parse_integer_entity<'i>(
+pub fn parse_float_entity<'i>(
     input: &mut &'i [u8],
 ) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parse_float_entity",
     (
-        preceded(b"\x02", parse_string),
-        binary::le_u32.map(MapValue::Number),
+        preceded(b"\x03", parse_string),
+        binary::le_f32.map(MapValue::NumberFloat),
     )
-        .context("parse_integer_entity")
+        .context("parse_float_entity"))
+        .parse_next(input)
+}
+
+pub fn parse_integer64_entity<'i>(
+    input: &mut &'i [u8],
+) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parse_integer64_entity",
+    (
+        preceded(b"\x07", parse_string),
+        binary::le_u64.map(MapValue::Number64),
+    )
+        .context("parse_integer64_entity"))
         .parse_next(input)
 }
 
 pub fn parse_string_entity<'i>(
     input: &mut &'i [u8],
 ) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parse_string_entity",
     (
         preceded(b"\x01", parse_string),
         parse_string.map(MapValue::String),
     )
-        .context("parse_string_entity")
+        .context("parse_string_entity"))
         .parse_next(input)
 }
 
 pub fn parse_hash_entity<'i>(
     input: &mut &'i [u8],
 ) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parseHashEntity",
     delimited(
-        literal([0x00]).context("start parse_hash_entity"),
+        b"\x00".context("start parse_hash_entity"),
         (
             parse_string.context("parse map name"),
             repeat(0.., 
-                parse_map_value)
+                parse_map_value).context("parse hash cotents")
                 .map(|x: Vec<(String, MapValue)>| 
                 MapValue::Object(x.into_iter().collect())),
-        ).context("parse hash cotents"),
-        b"\x08",
+        ).context("convert hash cotents"),
+        b"\x08".context("end parse_hash_entity"),
     )
-    .context("parse_hash_entity")
+    .context("parse_hash_entity"))
     .parse_next(input)
 }
 
@@ -71,18 +124,21 @@ pub fn parse_hash_entity<'i>(
 pub fn parse_map_value<'i>(
     input: &mut &'i [u8],
 ) -> PResult<(String, MapValue), ContextError<&'i str>> {
+    trace("parseMapEntity",
     dispatch!(peek(any);
         0x01 => parse_string_entity,
-        0x02 => parse_integer_entity,
+        0x02 => parse_integer32_entity,
+        0x03 => parse_float_entity,
+        0x07 => parse_integer64_entity,
         0x00 => parse_hash_entity,
-        _ => fail,
-    ).parse_next(input)
+        _ => fail.context("parse_map_value error"),
+    )).parse_next(input)
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_hash_entity, parse_integer_entity, parse_string_entity, MapValue};
+    use crate::{parse_hash_entity, parse_integer32_entity, parse_string_entity, MapValue};
     use std::collections::HashMap;
 
     #[test]
@@ -101,8 +157,8 @@ mod tests {
     #[test]
     fn parse_int_kv() {
         let input = &mut b"\x02appid\x00Jz\x86\xF4".as_slice();
-        match parse_integer_entity(input) {
-            Ok(actual) => assert_eq!(actual, ("appid".to_string(), MapValue::Number(4102453834))),
+        match parse_integer32_entity(input) {
+            Ok(actual) => assert_eq!(actual, ("appid".to_string(), MapValue::Number32(4102453834))),
             Err(err) => panic!("{}", err),
         }
     }
@@ -113,7 +169,7 @@ mod tests {
         let input= &mut &b"\x00shortcuts\x00\x02appid\x00\x51\x2D\xEB\x82\x01exe\0\"C:\\Program Files (x86)\\Games\\Game.exe\"\0\x08"[..];
 
         let mut expected: HashMap<String, MapValue> = HashMap::new();
-        expected.insert("appid".to_string(), MapValue::Number(2196450641));
+        expected.insert("appid".to_string(), MapValue::Number32(2196450641));
         expected.insert(
             "exe".to_string(),
             MapValue::String("\"C:\\Program Files (x86)\\Games\\Game.exe\"".to_string()),
